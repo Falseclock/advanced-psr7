7bac2708d803e23b30e352033bba0e3c677c741e
Falseclock <nurike@gmail.com>
2022-10-23 10:02:59 +0600

    xdebug

---
+++ src/HttpResponse.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Falseclock\AdvancedPSR7;
+
+use GuzzleHttp\Psr7\Response;
+
+class HttpResponse extends Response
+{
+
+}
---
+++ tests/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Special index file to test requests via HTTP
+ */
+
+declare(strict_types=1);
+
+namespace Falseclock\AdvancedPSR7\Tests;
+
+
+
+phpinfo();
